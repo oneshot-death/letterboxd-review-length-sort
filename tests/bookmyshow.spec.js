43547464d2206b11198194fe12edaba088506091
Samar <samarjomysujith606@gmail.com>
2025-08-23 12:29:16 +0530

    Added NodeMailer to get notification(s)

--- tests/bookmyshow.spec.js
+++ tests/bookmyshow.spec.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const { test, expect } = require('@playwright/test');
+const nodemailer = require('nodemailer');
 
 test('Checking if tickets are available for a film', async ({page}) => {
     await page.goto("https://in.bookmyshow.com/movies/bengaluru/coolie/ET00395817")
@@ -11,6 +13,7 @@ const button = page.locator('button:has-text("Book tickets")').first();
         //await page.waitForSelector('.sc-8f9mtj-0.sc-8f9mtj-1.sc-1vmod7e-0.cUKDnO.hxNpBB.fiWmpq', { state: 'visible' });
         const continueButton=await page.locator('div.sc-ttkokf-2.sc-ttkokf-3.bIIppr.hIYQmz');
         await continueButton.click();
+        await sendEmailNotification();
         //await page.waitForSelector('.sc-ttkokf-2 sc-ttkokf-3 bIIppr hIYQmz', { state: 'visible' });
         //await page.click(".sc-ttkokf-2 sc-ttkokf-3 bIIppr hIYQmz");
 
@@ -21,3 +24,30 @@ const button = page.locator('button:has-text("Book tickets")').first();
     }
     await page.close();
 });
+
+async function sendEmailNotification() {
+  
+  let transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS  
+    }
+  });
+
+
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,  // Replace with ur email
+    subject: 'Tickets are now available!',
+    text: 'The tickets for the film are now available. Please check it out on BookMyShow.'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+}
