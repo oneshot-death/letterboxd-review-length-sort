994fce98e56ccc85da7e2f7ad82128c3475231ba
Samar <samarjomysujith606@gmail.com>
2025-09-01 22:18:17 +0530

    Added json to check if film url is cached

--- tests/bookmyshow.spec.js
+++ tests/bookmyshow.spec.js
@@ -11,24 +11,51 @@ function getConfig() {
 }
 
 const config = getConfig();
-console.log("Using config:", config);
 
-test('Checking if tickets are available for a film', async ({page}) => {
-  await page.goto(bookingPage);
-  await page.locator('input[placeholder="Search for your city"]').fill(`${config.location}`);
-  await page.locator("xpath=/html/body/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div").click() //clicks the first input
-  await page.locator("xpath=/html/body/div[1]/div/div[2]/div[3]/div[1]/div[2]/div/div/div/div[1]/div[1]/div[2]/a/div").click() //see all
-  const movieLocator = page.locator(`a:has-text("hridayapoorvam")`).first();
+function saveMovieUrl(movieName, url) {
+  const file = "movies.json";
+  let data = {};
 
-  // keep scrolling until the element is visible
-  while (!(await movieLocator.isVisible())) {
-    await page.mouse.wheel(0, 300); // scroll down a bit
-    await page.waitForTimeout(500); // small delay (so it feels natural)
+  if (fs.existsSync(file)) {
+    data = JSON.parse(fs.readFileSync(file, "utf-8"));
   }
 
-  await movieLocator.click();
+  data[movieName] = url;
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
 
-  const button = await page.locator('button:has-text("Book tickets")').first();
+function getMovieUrl(movieName) {
+  const file = "movies.json";
+  if (!fs.existsSync(file)) return null;
+
+  const data = JSON.parse(fs.readFileSync(file, "utf-8"));
+  return data[movieName] || null;
+}
+
+test('Checking if tickets are available for a film', async ({page}) => {
+  let filmUrl=getMovieUrl(`${config.movie}`)
+  if (!filmUrl) {
+    await page.goto(bookingPage);
+    await page.locator('input[placeholder="Search for your city"]').fill(`${config.location}`);
+    await page.locator("xpath=/html/body/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div").click() //clicks the first input
+    await page.locator("xpath=/html/body/div[1]/div/div[2]/div[3]/div[1]/div[2]/div/div/div/div[1]/div[1]/div[2]/a/div").click() //see all
+    const movieLocator = page.locator(`a:has-text("${config.movie}")`).first();
+
+    // keep scrolling until the element is visible
+    while (!(await movieLocator.isVisible())) {
+      await page.mouse.wheel(0, 300); // scroll down a bit
+      await page.waitForTimeout(500); // small delay (so it feels natural)
+    }
+
+    await movieLocator.click();
+    filmUrl=page.url()
+    saveMovieUrl(`${config.movie}`,filmUrl)
+  }
+  else {
+    await page.goto(filmUrl)
+  }
+  const button = await page.locator('button:has-text("Book tickets")').first().click();
+  
 
   if (await button.isVisible()) {
     console.log('Tickets available now');
