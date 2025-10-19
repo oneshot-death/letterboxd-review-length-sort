c8f459e306923955610e078ee9d7910de19c2832
Samar <samarjomysujith606@gmail.com>
2025-10-19 10:30:18 +0530

    Going to fix repeating review error and next button checking soon

--- tests/letterboxd_review_length.spec.js
+++ tests/letterboxd_review_length.spec.js
@@ -1,20 +1,36 @@
 const { test, expect } = require('@playwright/test');
 const { count } = require('console');
+var fs=require('fs')
+let previous_review
 
-async function longReviewChecking(page) {
+async function longReviewChecking(page,minReviews,review_count,review_data) {
+  var fs = require('fs');
   const moreButton=await page.getByText("more")
+  previous_review=review_data
   for (let i=0;i<await moreButton.count()-1;i++) {
     await moreButton.nth(i).click()
-    console.log(await moreButton.first().textContent())
+    let review_data=await moreButton.nth(i).locator("..").textContent()
+    if (previous_review==review_data) {
+      review_count++
+      fs.appendFileSync('reviews.txt', `\n--- Review ${i} ---\n${review_data.trim()}\n`);
+    }
+  //  data.push({
+  //  review_number:review_count,
+  //  review:review_data
+  //  })
+  }
+  if (minReviews>review_count) {
+    await page.locator('.next').click()
+    await longReviewChecking(page,minReviews,review_count)
   }
 }
 
-test('has title', async ({ page }) => {
+test('review length', async ({ page }) => {
   await page.goto("https://letterboxd.com/", { waitUntil: 'domcontentloaded' })
   const search=await page.locator("id=search-q")
   await search.fill("one battle")
   await search.press("Enter")
   await page.locator(".film-title-wrapper").first().click()
   await page.locator(".all-link").first().click()
-  await longReviewChecking(page)
+  await longReviewChecking(page,5,0,null)
 });
